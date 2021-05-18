71e86be78d69409577e761159262aaa2e4e7b5bb
Parker J. Rule <parker.rule@tufts.edu>
2021-05-17 20:22:01 -0400

    Add working prefix sum tree (with tests)

---
+++ prefix_sum_tree.py
@@ -0,0 +1,219 @@
+from typing import Generic, TypeVar, Optional, Tuple, Any
+from range_scapegoat import RangeTree, RangeNode, RangeMeta, NodeType
+from collections import deque
+
+K = TypeVar('K')
+V = TypeVar('V')
+
+def make_meta(zero: Any):
+    """Generates a metadata class and rebuild function with a typed zero."""
+    class PrefixSumMeta(RangeMeta):
+        """Keeps track of metadata for min/max prefix sum lookups."""
+        def __init__(self, key: K, val: V, node: NodeType):
+            """Initializes range metadata."""
+            self.sum = val
+            self.min_prefix_sum = val
+            self.max_prefix_sum = val
+
+        def insert(self, key: K, val: V, node: NodeType) -> None:
+            """Updates range metadata to reflect an insert of a leaf."""
+            self.sum += val
+            self._propagate(node)
+
+        def remove(self, key: K, val: V, node: NodeType) -> None:
+            """Updates range metadata to reflect removal of a leaf."""
+            self.sum -= val
+            self._propagate(node)
+
+        def _propagate(self, parent: NodeType) -> None:
+            """Propagate changes upward."""
+            if parent:
+                self.min_prefix_sum = min(
+                    parent.left.meta.min_prefix_sum,
+                    parent.left.meta.sum + parent.right.meta.min_prefix_sum
+                )
+                parent.meta.max_prefix_sum = max(
+                    parent.left.meta.max_prefix_sum,
+                    parent.left.meta.sum + parent.right.meta.max_prefix_sum
+                )
+
+        def __repr__(self):
+            rep = f'sum: {self.sum}'
+            if self.min_prefix_sum is not None:
+                rep += f', subtree min prefix sum: {self.min_prefix_sum}'
+            if self.max_prefix_sum is not None:
+                rep += f', subtree max prefix sum: {self.max_prefix_sum}'
+            return rep
+
+    def prefix_sum_rebuild(root: NodeType) -> NodeType:
+        """Rebuilds from the bottom up in O(n) time."""
+        leaves = deque(leaf for _, leaf in root.leaves())
+        next_level = deque()
+        while leaves:
+            left = leaves.popleft()
+            if leaves:
+                right = leaves.popleft()
+                parent = RangeNode(left.min, None)
+                parent.left = left
+                parent.right = right
+                parent.max = right.max
+                parent.ub = left.ub + right.ub
+                parent.size = left.size + right.size
+                parent.meta = PrefixSumMeta(left.min, left.meta.sum, left)
+                parent.meta.sum = left.meta.sum + right.meta.sum
+                parent.meta.min_prefix_sum = min(
+                    left.meta.min_prefix_sum,
+                    left.meta.sum + right.meta.min_prefix_sum
+                )
+                parent.meta.max_prefix_sum = max(
+                    left.meta.max_prefix_sum,
+                    left.meta.sum + right.meta.max_prefix_sum
+                )
+                next_level.append(parent)
+            else:
+                next_level.append(left)
+            if not leaves and len(next_level) > 1:
+                leaves = next_level
+                next_level = deque()
+        return next_level.pop()
+
+    return PrefixSumMeta, prefix_sum_rebuild
+
+
+class PrefixSumTree(RangeTree[K, V]):
+    """A tree that supports prefix sum queries."""
+    def __init__(self, zero: Optional[V] = None):
+        PrefixSumMeta, prefix_rebuild = make_meta(zero)
+        super().__init__(rebuild_fn=prefix_rebuild,
+                         meta_cls=PrefixSumMeta)
+        self.zero = zero
+
+    def min_max_prefix_sum_at(self, key: K) -> Optional[Tuple[V, V]]:
+        """Finds the prefix sums at a node."""
+        path = list(self.root.path(key))
+        leaf = path[-1]
+        if leaf.is_leaf and leaf.min == key:
+            path_sum = self.zero
+            min_sum = self.zero
+            max_sum = self.zero
+            for (parent, child) in zip(path[:-1], path[1:]):
+                if child == parent.right:
+                    # Right turn.
+                    min_sum = min(
+                        min_sum, path_sum + parent.left.meta.min_prefix_sum
+                    )
+                    max_sum = max(
+                        max_sum, path_sum + parent.left.meta.max_prefix_sum
+                    )
+                    path_sum += parent.left.meta.sum
+            min_sum = min(min_sum, path_sum + leaf.meta.min_prefix_sum)
+            max_sum = max(max_sum, path_sum + leaf.meta.max_prefix_sum)
+            return min_sum, max_sum
+
+    def min_prefix_sum_at(self, key: K) -> Optional[V]:
+        """Finds the minimum prefix sum at a node."""
+        min_max = self.min_max_prefix_sum_at(key)
+        if min_max:
+            return min_max[0]
+
+    def max_prefix_sum_at(self, key: K) -> Optional[V]:
+        """Finds the maximum prefix sum at a node."""
+        min_max = self.min_max_prefix_sum_at(key)
+        if min_max:
+            return min_max[1]
+
+    def last_node_with_sum(self, key_ub: K, prefix_sum: V) -> Optional[NodeType]:
+        """Finds the last node such that:
+            * The key is ≤ key_ub.
+            * The prefix sum at the tree is exactly `prefix_sum`.
+
+        If such a node cannot be found, `None` is returned."""
+        path = list(self.root.path_left_biased(key_ub))
+        path_sum = self.zero
+        for parent, child in zip(path[:-1], path[1:]):
+            if child == parent.right:
+                path_sum += parent.left.meta.sum
+
+        # Hopefully, we got lucky and found a node with the right prefix sum.
+        # Otherwise, we work backward.
+        if prefix_sum == path_sum + path[-1].meta.sum:
+            return path[-1]
+
+        node = None
+        for parent, child in zip(reversed(path[:-1]), reversed(path[1:])):
+            left_lb = (path_sum - parent.left.meta.sum +
+                       parent.left.meta.min_prefix_sum)
+            left_ub = (path_sum - parent.left.meta.sum +
+                       parent.left.meta.max_prefix_sum)
+            if child == parent.right:
+                path_sum -= parent.left.meta.sum
+                # Move back up the tree until we find the first unexplored left
+                # subtree with the prefix sum in bounds.
+                if left_lb <= prefix_sum <= left_ub:
+                    node = parent.left
+                    break
+
+        # Walk to the rightmost possible node in the subtree we identified.
+        while node and not node.is_leaf:
+            left_sum = node.left.meta.sum
+            left_lb = node.left.meta.min_prefix_sum + path_sum
+            left_ub = node.left.meta.max_prefix_sum + path_sum
+            right_lb = node.right.meta.min_prefix_sum + path_sum + left_sum
+            right_ub = node.right.meta.max_prefix_sum + path_sum + left_sum
+            if node.right.min <= key_ub and \
+               right_lb <= prefix_sum <= right_ub:
+                node = node.right
+                path_sum += left_sum
+            elif left_lb <= prefix_sum <= left_ub:
+                node = node.left
+            else:
+                break
+        if node and node.is_leaf:
+            return node
+
+    def first_node_with_sum(self, key_lb: K, prefix_sum: V) -> Optional[NodeType]:
+        """Finds the last node such that:
+            * The key is ≥ key_lb.
+            * The prefix sum at the tree is exactly `prefix_sum`.
+
+        If such a node cannot be found, `None` is returned."""
+        path = list(self.root.path(key_lb))
+        path_sum = self.zero
+        for parent, child in zip(path[:-1], path[1:]):
+            if child == parent.right:
+                path_sum += parent.left.meta.sum
+
+        # Hopefully, we got lucky and found a node with the right prefix sum.
+        # Otherwise, we work backward.
+        if prefix_sum == path_sum + path[-1].meta.sum:
+            return path[-1]
+
+        node = None
+        for parent, child in zip(reversed(path[:-1]), reversed(path[1:])):
+            left_sum = parent.left.meta.sum
+            right_lb = path_sum + left_sum + parent.right.meta.min_prefix_sum
+            right_ub = path_sum + left_sum + parent.right.meta.max_prefix_sum
+            if child == parent.left and right_lb <= prefix_sum <= right_ub:
+                node = parent.right
+                path_sum += parent.left.meta.sum
+                break
+            if child == parent.right:
+                path_sum -= parent.left.meta.sum
+
+        # Go leftward as much as possible, going left when left.max < key_lb
+        # or the prefix sum constraint is violated.
+        while node and not node.is_leaf:
+            left_sum = node.left.meta.sum
+            left_lb = node.left.meta.min_prefix_sum + path_sum
+            left_ub = node.left.meta.max_prefix_sum + path_sum
+            right_lb = node.right.meta.min_prefix_sum + path_sum + left_sum
+            right_ub = node.right.meta.max_prefix_sum + path_sum + left_sum
+            if node.left.max >= key_lb and left_lb <= prefix_sum <= left_ub:
+                node = node.left
+            elif right_lb <= prefix_sum <= right_ub:
+                node = node.right
+                path_sum += left_sum
+            else:
+                break
+        if node and node.is_leaf:
+            return node
---
+++ test_prefix_sum.py
@@ -0,0 +1,92 @@
+"""Tests for the prefix sum tree.
+
+We particularly emphasize our use case: sequences with elements {-1, 0, 1}.
+"""
+import pytest
+from random import Random
+from collections import deque
+from prefix_sum_tree import PrefixSumTree
+
+
+def cumsum(x):
+    """The cumulative sum of an array."""
+    total = 0
+    sums = []
+    for el in x:
+        total += el
+        sums.append(total)
+    return sums
+
+
+def random_weight_seq(length, seed, lb=-1, ub=1):
+    rng = Random(seed)
+    return [rng.randint(lb, ub) for _ in range(length)]
+
+
+def make_random_weight_sequences(prefix, length, num_random, seed_offset=0):
+    return {
+        f'{prefix}_rand_{seed}': random_weight_seq(length, seed_offset + seed)
+        for seed in range(num_random)
+    }
+
+
+short_sequences = make_random_weight_sequences('short', 1000, 20)
+
+
+@pytest.mark.parametrize('seq_name', short_sequences.keys())
+def test_min_max_prefix_sum_at(seq_name):
+    seq = short_sequences[seq_name]
+    prefix_sums = cumsum(seq)
+    min_prefix_sums = [min(0, prefix_sums[0])]
+    max_prefix_sums = [max(prefix_sums[0], 0)]
+    for idx in range(1, len(seq)):
+        min_prefix_sums.append(min(min_prefix_sums[-1], prefix_sums[idx]))
+        max_prefix_sums.append(max(max_prefix_sums[-1], prefix_sums[idx]))
+
+    tree = PrefixSumTree(0)
+    for idx, val in enumerate(seq):
+        tree.insert(idx, val)
+    for idx in range(len(seq)):
+        min_prefix, max_prefix = tree.min_max_prefix_sum_at(idx)
+        assert min_prefix == min_prefix_sums[idx]
+        assert max_prefix == max_prefix_sums[idx]
+
+
+@pytest.mark.parametrize('seq_name', short_sequences.keys())
+def test_first_node_with_sum(seq_name):
+    seq = short_sequences[seq_name]
+    prefix_sums = cumsum(seq)
+    zero_indices = deque(idx for idx, s in enumerate(prefix_sums) if s == 0)
+
+    tree = PrefixSumTree(0)
+    for idx, val in enumerate(seq):
+        tree.insert(idx, val)
+    for idx in range(len(seq)):
+        print(idx)
+        first_node = tree.first_node_with_sum(idx, 0)
+        if zero_indices:
+            assert first_node.min == first_node.max == zero_indices[0]
+            if idx >= zero_indices[0]:
+                zero_indices.popleft()
+        else:
+            assert first_node is None
+
+
+@pytest.mark.parametrize('seq_name', short_sequences.keys())
+def test_last_node_with_sum(seq_name):
+    seq = short_sequences[seq_name]
+    prefix_sums = cumsum(seq)
+    zero_indices = deque(idx for idx, s in enumerate(prefix_sums) if s == 0)
+
+    tree = PrefixSumTree(0)
+    for idx, val in enumerate(seq):
+        tree.insert(idx, val)
+    for idx in range(len(seq) - 1, -1, -1):
+        last_node = tree.last_node_with_sum(idx, 0)
+        if zero_indices:
+            assert last_node.min == last_node.max == zero_indices[-1]
+            if idx <= zero_indices[-1]:
+                zero_indices.pop()
+        else:
+            assert last_node is None
+
